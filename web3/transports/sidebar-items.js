c1c1369a9f1f11bd5a6fb0b96c903f78255db476
Travis CI User <travis@example.org>
2017-09-15 07:43:20 +0000

    Update documentation

---
+++ web3/transports/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["batch","Batching Transport"],["http","HTTP Transport"],["ipc","IPC Transport for *nix"]],"struct":[["EventLoopHandle","Event Loop Handle. NOTE: Event loop is stopped when handle is dropped!"]],"type":[["Result","RPC Result."]]});
